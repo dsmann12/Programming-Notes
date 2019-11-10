d21b819ef7e93d2485f5c25d93adbef7507ad978
David Scheuermann <dsmann1212@gmail.com>
2019-11-10 09:41:10 -0600

    Added flutter tutorial code

---
+++ flutter/flutter_login_demo-master/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/home/david/.local/lib/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/david/Documents/Projects/Mobile/flutter/flutter_login_demo-master"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/home/david/.local/lib/flutter/bin/cache/artifacts/engine/ios"
